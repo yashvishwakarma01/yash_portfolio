1605fd01e103981ce0b2a284851d4bd61ec753c1
YashVishwakarma <yashvishwakarmaofficial@gmail.com>
2024-03-09 01:31:47 +0530

    changes done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,20 +22,20 @@ const Contact = () => {
           >
             <input
               type="text"
-              name={name}
+              name="name"
               placeholder="Enter your name"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
            
             />
             <input
               type="text"
-              name={email}
+              name='email'
               placeholder="Enter your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
           
             />
             <textarea
-              name={message}
+              name="message"
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
