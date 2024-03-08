cea003b6434655cbcbe454daad93da4d131c3c2a
YashVishwakarma <yashvishwakarmaofficial@gmail.com>
2024-03-09 00:44:43 +0530

    changes done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,6 @@
 import React, { useState } from "react";
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  // const body = { name, email, message };
-
-  // const url =
-  //   "https://script.google.com/macros/s/AKfycbwKWYkRFS7DwBiQZSPPiwNDnW7qm5T23DmFNSptOu8e8pLuMfPIP0Pn-aZRtXoH500s/exec";
-  // const handleSubmit = async (e) => {
-  //   const requestOptions = {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     // body: JSON.stringify(body),
-  //     body: body,
-  //   };
-  //   const response = await fetch(url, requestOptions);
-  //   const data = await response.json();
-
-  //   setName({ name: data.name });
-  //   setName({ name: data.email });
-  //   setName({ name: data.message });
-
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-    // let url="https://script.google.com/macros/s/AKfycbwiMA-FEQtou9BL_DEW2w2FbMmDdMzy0eoO0m2fonEOoduLn42tOzMDALlSbptYTmJdOQ/exec"
-    // let form=document.querySelector('form');
-    // form.addEventListener('submit',(e)=>{
-    //   e.target.btn="Submitting";
-    //   let d=new FormData(form);
-    //   fetch(url,{
-    //     method:'POST',
-    //     mode:'no-cors',
-    //     body:d
-    //   }).then((res)=>res.text())
-    //   .then((finalRes)=>{
-    //     e.target.btn="Let's Talk"
-    //     console.log(finalRes)
-    //   })
-    //   e.preventDefault()
-    // })
-  };
-  // };
   return (
     <div
       name="contact"
@@ -84,7 +43,6 @@ const Contact = () => {
             ></textarea>
 
             <button
-              type="submit"
               className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 btn"
             >
               Let's talk
@@ -94,7 +52,7 @@ const Contact = () => {
       </div>
     </div>
   );
-  // <script src="GoogleSheet.js"></script>
+
 };
 
 export default Contact;
